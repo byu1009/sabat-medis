340b2f22da2bbbf9ea5dc2b815cfb1976ba469d0
byu1009 <bayun1605@gmail.com>
2023-11-10 09:12:06 +0700

    add btn kiosk menu footer

--- resources/views/pages/extension/kiosk/clinic/index.blade.php
+++ resources/views/pages/extension/kiosk/clinic/index.blade.php
@@ -5,7 +5,14 @@
 @endsection
 
 @section('footer')
-    <div class="flex bg-yellow-300 justify-center">
-        Footer
+    <div class="flex bg-lime-200 py-3 justify-center">
+        <a href="{{ route('kiosk') }}">
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg class="w-3.5 h-3.5 mr-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+                    <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
+                </svg>
+                MENU UTAMA
+            </button>
+        </a>
     </div>
 @endsection
